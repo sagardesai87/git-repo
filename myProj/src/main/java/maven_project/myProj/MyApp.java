b0071e30a8264bafcadfcd5475fd64e257d3d64a
sagardesai87 <sagardesai1.8.87@gmail.com>
2017-01-22 19:56:31 +0530

    new file added

---
+++ myProj/src/main/java/maven_project/myProj/MyApp.java
@@ -0,0 +1,17 @@
+package maven_project.myProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyApp 
+{
+    public static void main( String[] args )
+    {
+    	//hello
+    	//hi
+        System.out.println( "Hello World! new text added" );
+        System.out.println("hi");
+
+    }
+}
