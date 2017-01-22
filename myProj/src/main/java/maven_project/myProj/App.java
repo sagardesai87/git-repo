31c0aea2641d5d3ce824cd2a67f630b5361b7996
Sagar <Sagar@Sagar-PC>
2017-01-22 19:57:46 +0530

    comment hi added

--- myProj/src/main/java/maven_project/myProj/App.java
+++ myProj/src/main/java/maven_project/myProj/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	//hello
-    	//hi
+    	//hi hi
         System.out.println( "Hello World! new text added" );
         System.out.println("hi");
 
