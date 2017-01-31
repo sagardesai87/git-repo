b9dd0fa2546a9984a73663799372ec8b98a57a7e
sagardesai87 <sagardesai1.8.87@gmail.com>
2017-01-31 12:38:18 +0530

    new comment added

--- myProj/src/main/java/maven_project/myProj/App.java
+++ myProj/src/main/java/maven_project/myProj/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	//hello
-    	//hi hi asdasdsadsad
+    	//hi hi asdasdsadsad asdasdasdsadsad
         System.out.println( "Hello World! new text" );
         System.out.println("hi");
 
