09c965b1991615876de7167216393abeaf99252e
sagar desai <sagar.desai@irisbusiness.com>
2017-01-31 12:41:28 +0530

    comment added

--- myProj/src/main/java/maven_project/myProj/App.java
+++ myProj/src/main/java/maven_project/myProj/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     	//hello
-    	//hi hi
+    	//hi hi asdasdsadsad
         System.out.println( "Hello World! new text" );
         System.out.println("hi");
 
